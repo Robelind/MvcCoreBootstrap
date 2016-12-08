0c17ae807df4871eceb807506c1848e0b847a146
Robert Lindblom <robert.lindblom@live.se>
2016-12-08 12:39:58 +0100

    Form rendering logic.

--- src/MvcCoreBootstrapForm/Rendering/FormRenderer.cs
+++ src/MvcCoreBootstrapForm/Rendering/FormRenderer.cs
@@ -1,10 +1,6 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Text.Encodings.Web;
-using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
 using MvcCoreBootstrap.Rendering;
 using MvcCoreBootstrapForm.Config;
@@ -21,70 +17,36 @@ namespace MvcCoreBootstrapForm.Rendering
         private readonly FormParameters _parameters;
         private readonly FormConfig _config;
         private readonly IHtmlHelper _htmlHelper;
-        private readonly IHtmlParser _htmlParser;
 
         public FormRenderer(FormParameters parameters)
         {
             _parameters = parameters;
             _config = _parameters.Config;
             _htmlHelper = parameters.HtmlHelper;
-            _htmlParser = parameters.Parser;
-        }
-
-        internal FormRenderer(FormConfig config, IHtmlHelper htmlHelper, IHtmlParser htmlParser)
-        {
-            _config = config;
-            _htmlHelper = htmlHelper;
-            _htmlParser = htmlParser;
         }
 
         public MvcForm Render()
         {
-            IList<string> content = new List<string>();
-            MvcForm form = this.CreateForm();
             ViewBufferTextWriter viewWriter = _htmlHelper.ViewContext.Writer as ViewBufferTextWriter;
-            string formTag = null;
-            StringWriter formWriter = new StringWriter();
-            TagBuilder formBuilder = null;
+            IDictionary<string, object> htmlAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(_parameters.HtmlAttributes);
 
-            Debug.Assert(viewWriter != null);
-            foreach(ViewBufferValue viewBufferValue in viewWriter.Buffer.Pages[0].Buffer.Where(b => b.Value != null))
+            // If so configured, add the appropriate class to the forms html attributes.
+            if(_config.ColumnWidths != null || _config.Inline)
             {
-                string value = viewBufferValue.Value.ToString();
+                string formClass = (_config.ColumnWidths != null ? "form-horizontal" : null) ?? "form-inline";
 
-                if(formTag != null)
+                htmlAttributes = htmlAttributes ?? new Dictionary<string, object>();
+                if(htmlAttributes.ContainsKey("class"))
                 {
-                    formTag += value;
-                    if(value == ">")
-                    {
-                        formBuilder = _htmlParser.TagBuilderFromHtmlContent(new HtmlString(formTag), false);
-                        formTag = null;
-                    }
-                }
-                else if(value != "<")
-                {
-                    content.Add(value);
+                    htmlAttributes["class"] += $" {formClass}";
                 }
                 else
                 {
-                    formTag = "<";
+                    htmlAttributes.Add("class", formClass);
                 }
             }
 
-            Debug.Assert(formBuilder != null);
-            this.AddCssClass("form-horizontal", _config.ColumnWidths != null, formBuilder);
-            this.AddCssClass("form-inline", _config.Inline, formBuilder);
-            formBuilder.TagRenderMode = TagRenderMode.StartTag;
-            formBuilder.WriteTo(formWriter, HtmlEncoder.Default);
-            content.Add(formWriter.ToString());
-
-            // Replace view writer content.
-            viewWriter.Buffer.Clear();
-            foreach(var s in content)
-            {
-                viewWriter.WriteLine(s);
-            }
-
+            // TODO: To js-file?
             // Add validation handling java script, if not already added.
             if(_htmlHelper.ViewBag.MvcBootStrapFormValJs == null)
             {
@@ -94,39 +56,14 @@ namespace MvcCoreBootstrapForm.Rendering
 
             _htmlHelper.ViewBag.MvcBootStrapFormColumnWidths = _config.ColumnWidths;
 
-            return(form);
-        }
-
-         private MvcForm CreateForm()
-        {
-            MvcForm form;
-
-            if(_parameters.AntiForgery.HasValue)
-            {
-                form = _htmlHelper.BeginForm(_parameters.Method, _parameters.AntiForgery,
-                    _parameters.HtmlAttributes);
-            }
-            else if(_parameters.HtmlAttributes != null)
-            {
-                form = _htmlHelper.BeginForm(_parameters.ActionName, _parameters.ControllerName, _parameters.Method,
-                    _parameters.HtmlAttributes);
-            }
-            else if(_parameters.RouteValues != null)
-            {
-                form = _htmlHelper.BeginForm(_parameters.ActionName, _parameters.ControllerName, _parameters.Method,
-                    _parameters.RouteValues);
-            }
-            else
-            {
-                form = _htmlHelper.BeginForm(_parameters.Method);
-            }
-
-            return(form);
+            return(_htmlHelper.BeginForm(_parameters.ActionName, _parameters.ControllerName,
+                    _parameters.RouteValues, _parameters.Method, _parameters.AntiForgery, htmlAttributes));
         }
 
         private string ValidationJs { get; } =
     @"<script type=""text/javascript"">
         $(document).ready(function() {
+            $('.field-validation-valid').addClass('text-danger');
             $('form').bind('invalid-form.validate', function (a, b) {
                 $('.has-error').removeClass('has-error');
                 $.each(b.errorList, function () {
